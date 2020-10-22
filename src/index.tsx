6918a9ced947aa2e1217d104f938e66c7c069bd2
Rob Gries <robert.w.gries@gmail.com>
2020-10-22 19:15:51 -0400

    Initial maps

--- src/index.tsx
+++ src/index.tsx
@@ -3,38 +3,41 @@ import { Camera, MapViewProps, Region } from 'react-native-maps';
 import { useJsApiLoader, GoogleMap } from '@react-google-maps/api';
 
 interface WebMapViewProps extends MapViewProps{
-  children: React.ReactNode,
+  children?: React.ReactNode,
   googleMapsApiKey: string,
 }
 
 export default function MapView({ children, googleMapsApiKey, ...props }: WebMapViewProps) {
   const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey
+    googleMapsApiKey,
   });
 
   const center = getCenter(props.camera, props.initialCamera, props.region, props.initialRegion);
+  const zoom = props.camera ? props.camera.zoom : 0;
+  const tilt = props.camera ? props.camera.pitch : 0;
   const renderMap = () => (
-    <GoogleMap center={center} onLoad={props.onMapReady}>
+    <GoogleMap center={center} zoom={zoom} tilt={tilt} onLoad={props.onMapReady}>
       {children}
     </GoogleMap>
   );
 
   if (loadError) {
+    console.log(loadError);
     return null;
   }
 
   return isLoaded ? renderMap() : null;
 }
 
-function getCenter(camera?: Camera, initialCamera?: Camera, region?: Region, initialRegion?: Region) {
+function getCenter(camera?: Camera, initialCamera?: Camera, region?: Region, initialRegion?: Region): google.maps.LatLngLiteral | undefined {
   if (camera) {
-    return camera.center;
+    return { lat: camera.center.latitude, lng: camera.center.longitude };
   } else if (initialCamera) {
-    return initialCamera.center;
+    return { lat: initialCamera.center.latitude, lng: initialCamera.center.longitude };
   } else if (region) {
-    return { latitude: region.latitude, longitude: region.longitude };
+    return { lat: region.latitude, lng: region.longitude };
   } else if (initialRegion) {
-    return { latitude: initialRegion.latitude, longitude: initialRegion.longitude };
+    return { lat: initialRegion.latitude, lng: initialRegion.longitude };
   }
 }
 
